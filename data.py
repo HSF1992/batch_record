40243330454fc4e026435110cebe4e601eb5ed7b
HSF1992 <525157029@qq.com>
2026-08-27 14:30:54 +0800

    Add function to find batch number in CSV

--- data.py
+++ data.py
@@ -12,5 +12,17 @@ def 追加记录(批号,收率,结果):
         if 文件.tell()==0:
             写入器.writerow(["批号","收率","结果"])
         写入器.writerow([批号,收率,结果])
-    
+def 查找批号(批号):
+    try:
+        with open("批记录.csv",mode="r",encoding="utf-8") as 文件:
+            reader=csv.reader(文件)
+            表头_=next(reader)
+            for i in reader:
+                if i[0]==批号:
+                    批信息=[批号,i[1],i[2]]
+                    return 批信息
+                return []
+    except FileNotFoundError:
+        return []
+ 
         
